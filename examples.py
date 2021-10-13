360acb6fc09791962e5c431f5e0ed55db2f0623c
MohanBailapudi <36395532+MohanBailapudi@users.noreply.github.com>
2021-10-13 17:30:09 +0530

    Added Comments and few checks

--- examples.py
+++ examples.py
@@ -5,12 +5,14 @@ from pydeequ.profiles import *
 from pydeequ.checks import *
 from pydeequ.verification import *
 
+## Creating Spark Session
 spark = (SparkSession
     .builder
     .config("spark.jars.packages", pydeequ.deequ_maven_coord)
     .config("spark.jars.excludes", pydeequ.f2j_maven_coord)
     .getOrCreate())
 
+## Creating Sample DataFrame
 df = spark.sparkContext.parallelize([
             Row(emp_name="mohan1", emp_age=21, emp_sal=90,emp_dept = 'Science',emp_email='mohan1@gmail.com'),
             Row(emp_name="mohan2", emp_age=22, emp_sal=900,emp_dept = 'Scocial',emp_email='mohan2@gmail.com'),
@@ -31,6 +33,7 @@ df = spark.sparkContext.parallelize([
             Row(emp_name="mohan17", emp_age=27, emp_sal=950,emp_dept = 'Scocial',emp_email='mohan1@gmail.com'),
             Row(emp_name="mohan18", emp_age=28, emp_sal=None,emp_dept = 'Biology',emp_email='mohan1@gmail.com')]).toDF()
 
+## Analzises the data for examples Completeness,Distinctness,Datatype etc...
 analysisResult = AnalysisRunner(spark) \
                     .onData(df) \
                     .addAnalyzer(Size()) \
@@ -40,16 +43,18 @@ analysisResult = AnalysisRunner(spark) \
 analysisResult_df = AnalyzerContext.successMetricsAsDataFrame(spark, analysisResult)
 analysisResult_df.show()
 
-### Complete Profile on dataframe
+### Complete Profile on dataframe - Column level analysis is provided by this method
 result = ColumnProfilerRunner(spark) \
     .onData(df) \
     .run()
 
+### Looping through the result object and printing the profile of each column
 for col, profile in result.profiles.items():
     print(profile)
 
 
-### Constraints
+### Constraints Checks
+###'''Below example is cheking on completeness, uniqueness,allowed values, email pattern, datatype'''
 check = Check(spark, CheckLevel.Warning, "Review Check")
 
 checkResult = VerificationSuite(spark) \
@@ -59,9 +64,14 @@ checkResult = VerificationSuite(spark) \
         .isComplete("emp_sal")  \
         .isUnique("emp_name")  \
         .isContainedIn("emp_dept", ["Scocial", "Science", "Maths"]) \
-        .isNonNegative("emp_age")) \
-        .containsEmail("emp_email")
-    .run()
+        .isNonNegative("emp_age") \
+        .containsEmail("emp_email") \
+        .hasDataType("emp_age",int,assertion=lambda x: x == 1.0) \
+        .hasPattern(column='emp_email',
+                     pattern=r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""",
+                     assertion=lambda x: x == 1.0)
+).run()
 
+### Converting the result to dataframe
 checkResult_df = VerificationResult.checkResultsAsDataFrame(spark, checkResult)
 checkResult_df.show()
