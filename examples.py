de6482ae698c0da9ddffc0f485b3d42b45866f2d
MohanBailapudi <36395532+MohanBailapudi@users.noreply.github.com>
2021-10-14 07:38:30 +0530

    Fixed Datatype Consstraint

--- examples.py
+++ examples.py
@@ -66,11 +66,8 @@ checkResult = VerificationSuite(spark) \
         .isContainedIn("emp_dept", ["Scocial", "Science", "Maths"]) \
         .isNonNegative("emp_age") \
         .containsEmail("emp_email") \
-        .hasDataType("emp_age",int,assertion=lambda x: x == 1.0) \
-        .hasPattern(column='emp_email',
-                     pattern=r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""",
-                     assertion=lambda x: x == 1.0)
-).run()
+        .hasDataType("emp_age",ConstrainableDataTyoes.Integral,assertion=lambda x: x == 1.0) \
+        ).run()
 
 ### Converting the result to dataframe
 checkResult_df = VerificationResult.checkResultsAsDataFrame(spark, checkResult)
