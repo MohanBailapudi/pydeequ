2d73ed6537f5555fb06be1bce57ed0382a30ae4d
MohanBailapudi <36395532+MohanBailapudi@users.noreply.github.com>
2021-10-25 08:15:58 +0530

    Create data_frame.py

---
+++ data_frame.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession, Row
+df_rows = [
+            Row(emp_name="mohan1", emp_age=21, emp_sal=90,emp_dept = 'Science',emp_email='mohan1@gmail.com'),
+            Row(emp_name="mohan2", emp_age=22, emp_sal=900,emp_dept = 'Scocial',emp_email='mohan2@gmail.com'),
+            Row(emp_name="mohan3", emp_age=23, emp_sal=9000,emp_dept = 'Maths',emp_email='mohan3@gmail.com'),
+            Row(emp_name="mohan4", emp_age=24, emp_sal=90000,emp_dept = 'Biology',emp_email='mohan4@gmail.com'),
+            Row(emp_name="mohan5", emp_age=25, emp_sal=900000,emp_dept = 'Science',emp_email='mohan5@gmail.com'),
+            Row(emp_name="mohan6", emp_age=23, emp_sal=10,emp_dept = 'Maths',emp_email='mohan6@gmail'),
+            Row(emp_name="mohan7", emp_age=24, emp_sal=190,emp_dept = 'Biology',emp_email='mohan1@gmail.com'),
+            Row(emp_name="mohan8", emp_age=25, emp_sal=9220,emp_dept = 'Science',emp_email='mohan1@gmail.com'),
+            Row(emp_name="mohan9", emp_age=24, emp_sal=9680,emp_dept = 'Maths',emp_email='mohan7@gmail.com'),
+            Row(emp_name="mohan10", emp_age=-25, emp_sal=958560,emp_dept = 'Science',emp_email='mohan1@gmail.com'),
+            Row(emp_name="mohan11", emp_age=25, emp_sal='',emp_dept = 'Scocial',emp_email='mohan1@gmail.com'),
+            Row(emp_name="mohan12", emp_age=26, emp_sal=9585680,emp_dept = 'Biology',emp_email='mohan1@gmail.com'),
+            Row(emp_name="mohan13", emp_age=27, emp_sal=None,emp_dept = 'Science',emp_email='mohan9@gmail.com'),
+            Row(emp_name="mohan14", emp_age=28, emp_sal=958580,emp_dept = 'Maths',emp_email='mohan1@gmail'),
+            Row(emp_name="mohan15", emp_age=25, emp_sal=9585680,emp_dept = 'Biology',emp_email='mohan1@gmail.com'),
+            Row(emp_name="mohan16", emp_age=26, emp_sal=950,emp_dept = 'Science',emp_email='mohan1gmail.com'),
+            Row(emp_name="mohan17", emp_age=27, emp_sal=950,emp_dept = 'Scocial',emp_email='mohan1@gmail.com'),
+            Row(emp_name="mohan18", emp_age=28, emp_sal=None,emp_dept = 'Biology',emp_email='mohan1@gmail.com')]
